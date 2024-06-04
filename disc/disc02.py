050f95af319e420850e195d5b0476798f8b54d68
cheng <517674583@qq.com>
2024-06-04 20:46:24 +0800

    disc02 finished

---
+++ disc/disc02.py
@@ -0,0 +1,45 @@
+def make_keeper(n):
+    """Returns a function that takes one parameter cond and prints
+    out all integers 1..i..n where calling cond(i) returns True.
+
+    >>> def is_even(x): # Even numbers have remainder 0 when divided by 2.
+    ...     return x % 2 == 0
+    >>> make_keeper(5)(is_even)
+    2
+    4
+    >>> make_keeper(5)(lambda x: True)
+    1
+    2
+    3
+    4
+    5
+    >>> make_keeper(5)(lambda x: False)  # Nothing is printed
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(cond):
+        for i in range(1, n + 1):
+            if cond(i):
+                print(i)
+    return helper
+
+def find_digit(k):
+    """Returns a function that returns the kth digit of x.
+
+    >>> find_digit(2)(3456)
+    5
+    >>> find_digit(2)(5678)
+    7
+    >>> find_digit(1)(10)
+    0
+    >>> find_digit(4)(789)
+    0
+    """
+    assert k > 0
+    "*** YOUR CODE HERE ***"
+    def helper(x):
+        if x < 10**k:
+            return 0
+        while x >= 10**(k + 1):
+            x //= 10
+        return x % 10
+    return helper
